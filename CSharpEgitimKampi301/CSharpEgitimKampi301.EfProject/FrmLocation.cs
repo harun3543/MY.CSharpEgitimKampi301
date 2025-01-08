2d6eb3fa79ea1d89ab54448c1753672cc8ae5170
Harun Durmuş <harun.3543@gmail.com>
2025-01-08 20:23:37 +0300

    08.01.2025 - Video 17 tamamlandı...

---
+++ CSharpEgitimKampi301/CSharpEgitimKampi301.EfProject/FrmLocation.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.EfProject
+{
+    public partial class FrmLocation : Form
+    {
+        public FrmLocation()
+        {
+            InitializeComponent();
+            this.Load += FrmLocation_Load;
+        }
+
+        EgitimKampiEfTravelDbEntities1 db = new EgitimKampiEfTravelDbEntities1();
+
+        private void FrmLocation_Load(object sender, EventArgs e)
+        {
+            //var values = db.Guides.ToList();
+            // Aşağıdaki select Linq sorgusu ile Fullname isminde yeni bir variable oluşturup
+            // guide tablosundaki "Name" "Surname" değikenlerini birleştirmiş olduk
+            // Ardından combobax aracına bu "Fullname" değişkenini verdik.
+            var values = db.Guides.Select(x=> new
+            {
+                Fullname = x.Name + " " + x.Surname,
+                x.GuideId
+            }).ToList();
+
+            cmbGuide.DisplayMember = "Fullname";
+            cmbGuide.ValueMember = "GuideId";
+            cmbGuide.DataSource = values;
+        }
+
+        
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = db.Locations.ToList();
+            dataGridView1 .DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            db.Locations.Add(new Location
+            {
+                City = txtCity.Text,
+                Country = txtCountry.Text,
+                Capacity = byte.Parse(nudCapacity.Value.ToString()),
+                Price = decimal.Parse(txtPrice.Text),
+                DayNight = txtDayNight.Text,
+                GuideId = int.Parse(cmbGuide.SelectedValue.ToString())
+            });
+            db.SaveChanges();
+            MessageBox.Show("Kayıt ekleme başarılı...","Uyarı",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var id = int.Parse(txtLocationId.Text);
+            var values = db.Locations.Find(id);
+            db.Locations.Remove(values);
+            db.SaveChanges();
+            MessageBox.Show("Kayıt silme başarılı...", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            var id = int.Parse(txtLocationId.Text);
+            var values = db.Locations.Find(id);
+            values.City = txtCity.Text;
+            values.Country = txtCountry.Text;
+            values.Capacity =byte.Parse(nudCapacity.Value.ToString());
+            values.Price = decimal.Parse(txtPrice.Text);
+            values.DayNight = txtDayNight.Text;
+            values.GuideId = int.Parse(cmbGuide.SelectedValue.ToString());
+            db.SaveChanges();
+            MessageBox.Show("Kayıt güncelleme başarılı...", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
